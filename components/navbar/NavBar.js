12644e329c4b982005436e6b60ce9b6268a3cb36
dom <d.amrugiewicz@sirup.com>
2021-11-04 10:00:15 +0100

    adjust navbar mobile

--- components/navbar/NavBar.js
+++ components/navbar/NavBar.js
@@ -3,20 +3,15 @@ import PropTypes from 'prop-types';
 import NavBarList from "./NavBarList";
 
 const NavBar = ({navOpen, setNavOpen}) => (
-	<div className={`transition-transform transform ease-in ease-out duration-300 ${navOpen ? "" : "translate-x-full"} 
+	<div className={`transition-transform transform ease-in ease-out duration-300 ${navOpen ? "" : "-translate-y-full md:-translate-y-0"} 
                     md:translate-x-0 md:block 
                     fixed md:static 
-                    bg-primary md:bg-transparent 
+                    bg-dark md:bg-transparent 
                     left-0 right-0 top-0 bottom-0
-                    text-dark md:text-white
+                    text-primary md:text-white
                     p-5 md:py-0
+                    border-b-2 border-primary md:border-0
                     `}>
-
-		<button onClick={() => setNavOpen(!navOpen)} type="button" className={"md:hidden"}>
-			<span className="sr-only">Close Menu</span>
-			<div className={"w-8 rounded h-0.5 bg-white my-1.5 transform origin-top-left rotate-45"}/>
-			<div className={"w-8 rounded h-0.5 bg-white mt-5 transform origin-top-left -rotate-45"}/>
-		</button>
 
 		<NavBarList navOpen={navOpen} setNavOpen={setNavOpen}/>
 	</div>
--- components/navbar/NavBarList.js
+++ components/navbar/NavBarList.js
@@ -4,12 +4,19 @@ import NavBarListItem from "./NavBarListItem";
 
 const NavBarList = ({setNavOpen, navOpen}) => {
 	return (
-		<ul className={"md:flex"}>
-			<NavBarListItem anchor={"#start"} title={"start"} navOpen={navOpen} setNavOpen={setNavOpen}/>
-			<NavBarListItem anchor={"#about"} title={"about"} navOpen={navOpen} setNavOpen={setNavOpen}/>
-			<NavBarListItem anchor={"#skills"} title={"skills"} navOpen={navOpen} setNavOpen={setNavOpen}/>
-			<NavBarListItem anchor={"#github"} title={"github"} navOpen={navOpen} setNavOpen={setNavOpen}/>
-			<NavBarListItem cta={true} anchor={"#contact"} title={"hire me"} navOpen={navOpen} setNavOpen={setNavOpen}/>
+		<ul className={`md:flex`}>
+			<li className={`px-2 mt-10 md:mt-0 text-right text-4xl md:text-base`}>
+				<button onClick={() => setNavOpen(!navOpen)} type={`button`} className={`md:hidden`}>
+					<span className={`sr-only`}>Close Menu</span>
+					<div className={`w-8 rounded h-0.5 bg-white my-1.5 transform origin-top-left rotate-45`}/>
+					<div className={`w-8 rounded h-0.5 bg-white mt-5 transform origin-top-left -rotate-45`}/>
+				</button>
+			</li>
+			<NavBarListItem anchor={`#start`} title={`start`} navOpen={navOpen} setNavOpen={setNavOpen}/>
+			<NavBarListItem anchor={`#about`} title={`about`} navOpen={navOpen} setNavOpen={setNavOpen}/>
+			<NavBarListItem anchor={`#skills`} title={`skills`} navOpen={navOpen} setNavOpen={setNavOpen}/>
+			<NavBarListItem anchor={`#github`} title={`github`} navOpen={navOpen} setNavOpen={setNavOpen}/>
+			<NavBarListItem cta={true} anchor={`#contact`} title={`hire me`} navOpen={navOpen} setNavOpen={setNavOpen}/>
 		</ul>
 	);
 };
