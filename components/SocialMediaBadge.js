e1b9444a91be92693d86d8a2c9884cb53a9a5030
dom <d.amrugiewicz@sirup.com>
2021-12-01 08:36:49 +0100

    add react icons

--- components/SocialMediaBadge.js
+++ components/SocialMediaBadge.js
@@ -1,4 +1,8 @@
 import React, {useState} from 'react';
+import {FaTwitter} from "@react-icons/all-files/fa/FaTwitter";
+import {FaGithub} from "@react-icons/all-files/fa/FaGithub";
+import {FaShareAlt} from "@react-icons/all-files/fa/FaShareAlt";
+import {FaTimes} from "@react-icons/all-files/fa/FaTimes";
 
 const SocialMediaBadge = () => {
 	const [show, setShow] = useState(false)
@@ -8,21 +12,23 @@ const SocialMediaBadge = () => {
 			<a href={"https://twitter.com/Dom_TheDev"}
 			   rel="noreferrer noopener"
 			   target="_blank"
-			   className={`flex justify-center items-center w-10 h-10 bg-primary absolute transition-all ${show ? " -top-16 opacity-100" : "top-0 opacity-0 pointer-events-none"}`}
+			   title="Twitter"
+			   className={`text-xs flex justify-center items-center w-10 h-10 bg-primary absolute transition-all ${show ? " -top-16 opacity-100" : "top-0 opacity-0 pointer-events-none"}`}
 			   style={{backgroundColor: '#1A8CD8'}}
 			>
 				<span className="sr-only">Twitter</span>
-				T
+				<FaTwitter />
 			</a>
 
 			<a href={"https://github.com/dom-the-dev"}
 			   rel="noreferrer noopener"
 			   target="_blank"
+			   title="GitHub"
 			   className={`flex justify-center items-center w-10 h-10 absolute transition-all ${show ? "right-7  opacity-100" : "right-0  opacity-0 pointer-events-none"}`}
 			   style={{backgroundColor: '#161B22'}}
 			>
 				<span className="sr-only">GitHub</span>
-				G
+				<FaGithub />
 			</a>
 
 			<div className={`group absolute w-10 h-10 bg-primary ${show && "animate-spin-slow"}`}>
@@ -30,10 +36,21 @@ const SocialMediaBadge = () => {
 
 			<button
 				type="button"
+				title="toggle social media icons"
 				onClick={() => setShow(!show)}
-				className={`absolute text-white w-10 h-10 flex justify-center items-center transform -rotate-12 hover:text-dark ${show && "text-dark"}`}>
+				className={`absolute text-dark w-10 h-10 flex justify-center items-center transform -rotate-12 hover:text-white ${show && "text-dark"}`}>
 				<span className="sr-only">Social Media Menu</span>
-				{show ? "X" : "S"}
+				{show ?
+					<>
+						<div className="sr-only">close social media icons</div>
+						<FaTimes />
+					</>
+					:
+					<>
+						<div className="sr-only">open social media icons</div>
+						<FaShareAlt />
+					</>
+				}
 			</button>
 
 		</div>
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import Layout from "../components/layout/Layout";
 import Section from "../components/layout/Section";
 import ListRepos from "../components/list/ListRepos";
-import Link from 'next/link'
 import ContactForm from "../components/ContactForm";
 
 export default function Home({repos}) {
@@ -10,50 +9,59 @@ export default function Home({repos}) {
 		<Layout title={"Home"}>
 			<Section id={"start"}>
 				<span className={"text-3xl"}>Welcome, I&apos;m</span>
-				<h1><span className={"font-bold"}>dom</span> <span className={"text-primary font-light"}>the dev</span></h1>
+				<h1><span className={"font-bold"}>dom</span> <span className={"text-primary font-light"}>the dev</span>
+				</h1>
 				<p className={"text-3xl"}>A <span className="font-bold">Fullstack JavaScript Developer</span> never
 					tired of learning and always up for new projects. <a
 						href="#contact"><span className={"text-primary font-bold"}>#hmu</span></a></p>
 			</Section>
 
+			<div className={"text-xs"}>
+
+			</div>
+
 			<Section id={"about"} title={"about."}>
-					<p>
-						Hi, I&apos;m <span className="font-bold">Dom</span> and I&apos;m a full stack developer from <span
-						className="font-bold">Germany.</span>
-					</p>
-					<p>
-						After a <span className="font-bold">vocational training</span> in Germany&apos;s capital city Berlin,
-						I started my career as a <span className="font-bold">frontend
+				<p>
+					Hi, I&apos;m <span className="font-bold">Dom</span> and I&apos;m a full stack developer from <span
+					className="font-bold">Germany.</span>
+				</p>
+
+				<p>
+					After a <span className="font-bold">vocational training</span> in Germany&apos;s capital city
+					Berlin,
+					I started my career as a <span className="font-bold">frontend
 						developer</span> at the agency <span className="font-bold">SIRUP</span>.
-					</p>
+				</p>
 
-					<p>
-						Five beautiful years as a frontend developer i decided to take the next step and started focusing on learning full stack development.
-					</p>
+				<p>
+					Five beautiful years as a frontend developer i decided to take the next step and started focusing on
+					learning full stack development.
+				</p>
 
-					<p>
-						Soon I passed the <span className="font-bold">Full Stack JavaScript Developer Nanodegree</span> at <span
-						className="font-bold">Udacity</span> and started to call myself a Full Stack Developer.
-					</p>
+				<p>
+					Soon I passed the <span
+					className="font-bold">Full Stack JavaScript Developer Nanodegree</span> at <span
+					className="font-bold">Udacity</span> and started to call myself a Full Stack Developer.
+				</p>
 
-					<p>
-						Since 2021 I am also working <span className="font-bold">self-employed</span> as a <span
-						className="font-bold">full stack javascript developer</span>, <span
-						className="font-bold">never tired</span> of learning and always up for
-						<span className="font-bold"> new projects</span>.
-					</p>
+				<p>
+					Since 2021 I am also working <span className="font-bold">self-employed</span> as a <span
+					className="font-bold">full stack javascript developer</span>, <span
+					className="font-bold">never tired</span> of learning and always up for
+					<span className="font-bold"> new projects</span>.
+				</p>
 
-					{/*<p>*/}
-					{/*	On this website you can see the stuff i am working on beside my work. Be sure to check out my latest project <a*/}
-					{/*	href="https://react-vs-vanilla.vercel.app/" target={"_blank"} className={"text-primary font-bold"} rel={"noreferrer"}>React vs. Vanilla JS</a>*/}
-					{/*</p>*/}
+				{/*<p>*/}
+				{/*	On this website you can see the stuff i am working on beside my work. Be sure to check out my latest project <a*/}
+				{/*	href="https://react-vs-vanilla.vercel.app/" target={"_blank"} className={"text-primary font-bold"} rel={"noreferrer"}>React vs. Vanilla JS</a>*/}
+				{/*</p>*/}
 
-					<p>
-						If you are interested in working with me, feel free to use the <a
-						href="#contact"><span className="font-bold text-primary">contact form</span></a> or write an
-						email to <span
-						className="font-bold text-primary"><a href="mailto:hi@domthedev.com">hi@domthedev.com</a></span>.
-					</p>
+				<p>
+					If you are interested in working with me, feel free to use the <a
+					href="#contact"><span className="font-bold text-primary">contact form</span></a> or write an
+					email to <span
+					className="font-bold text-primary"><a href="mailto:hi@domthedev.com">hi@domthedev.com</a></span>.
+				</p>
 			</Section>
 
 			{/*<Section id={"react-vs-vanilla"} title={"react-vs-vanilla."}>*/}
