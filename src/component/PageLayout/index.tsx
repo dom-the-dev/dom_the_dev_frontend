7df0bd144352f6def5170776fd9d05dd55cc77b6
dominikamrugiewicz <dominik.amrugiewicz@Wohnsinn.com>
2024-03-01 05:16:10 +0100

    fertig

---
+++ src/component/PageLayout/index.tsx
@@ -0,0 +1,60 @@
+import React, { FC, PropsWithChildren, PropsWithRef } from 'react';
+import Head from 'next/head';
+// import styles from '@/styles/Home.module.scss';
+import Link from 'next/link';
+import { Roboto_Slab, Roboto } from '@next/font/google';
+import styles from './PageLayout.module.scss';
+
+const robotoSlab = Roboto_Slab({ subsets: ['latin'] });
+const roboto = Roboto({ weight: '300', subsets: ['latin'] });
+
+const PageLayout: FC<PropsWithChildren<{ pageTitle: string }>> = ({ pageTitle, children }) => {
+  return (
+    <div className={`${roboto.className} ${styles.wrapper}`}>
+      <Head>
+        <title>{pageTitle} | dom the dev</title>
+        <meta name="description" content="Dom the devs homepage" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header className={`${robotoSlab.className} ${styles.header}`}>
+        <Link href={'/'}>
+          <h1>hi, i'm dom the dev</h1>
+        </Link>
+      </header>
+      <main className={styles.main}>{children}</main>
+      <footer className={`${robotoSlab.className} ${styles.footer}`}>
+        <ul>
+          <li>
+            <Link href={'/imprint'}>imprint</Link>
+          </li>
+          <li>
+            <a
+              target={'_blank'}
+              rel="noopener noreferrer"
+              href="https://www.youtube.com/domthedeveloper">
+              youtube
+            </a>
+          </li>
+          <li>
+            <a target={'_blank'} rel="noopener noreferrer" href="https://github.com/dom-the-dev/">
+              github
+            </a>
+          </li>
+          <li>
+            <a target={'_blank'} rel="noopener noreferrer" href="https://dev.to/dom_the_dev">
+              dev.to
+            </a>
+          </li>
+          <li>
+            <a target={'_blank'} rel="noopener noreferrer" href="mailto:hi@domthedev.com">
+              contact
+            </a>
+          </li>
+        </ul>
+      </footer>
+    </div>
+  );
+};
+
+export default PageLayout;
---
+++ src/pages/imprint/index.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { Roboto_Slab, Roboto } from '@next/font/google';
+import styles from '@/styles/Home.module.scss';
+import PageLayout from '@/component/PageLayout';
+
+const robotoSlab = Roboto_Slab({ subsets: ['latin'] });
+const roboto = Roboto({ weight: '300', subsets: ['latin'] });
+
+const Imprint = () => {
+  return (
+    <PageLayout pageTitle={'Imprint'}>
+      <h1>Impressum</h1>
+      <p>
+        Angaben gemäß § 5 TMG: <br />
+        Dominik Amrugiewicz <br />
+        hi@domthedev.com <br />
+        +49 176 329 029 67 <br />
+        <br />
+        67547 Worms
+      </p>
+      <h2>Haftungsausschluss (Disclaimer)</h2>
+      <h3>Haftung für Inhalte</h3>
+      <p>
+        Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach
+        den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter
+        jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen
+        oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen.
+        Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den
+        allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst
+        ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden
+        von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen. Haftung
+        für Links Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir
+        keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr
+        übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder
+        Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der
+        Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt
+        der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten
+        ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei
+        Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.
+      </p>
+
+      <h3>Urheberrecht</h3>
+
+      <p>
+        Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem
+        deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der
+        Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung
+        des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den
+        privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht
+        vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere
+        werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine
+        Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei
+        Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.
+      </p>
+
+      <h3>Datenschutzhinweis:</h3>
+
+      <p>
+        Beim Besuch dieser Internet-Seiten werden personenbezogene Daten erfasst, wenn Sie diese
+        Angaben freiwillig, etwa im Rahmen der Kontaktanfrage per E-Mail, machen und dienen zur
+        Beantwortung Ihrer Kontaktanfrage. Eine Weitergabe an Dritte findet nicht statt.
+      </p>
+    </PageLayout>
+  );
+};
+
+export default Imprint;
